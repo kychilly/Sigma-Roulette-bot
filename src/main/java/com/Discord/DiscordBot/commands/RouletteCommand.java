9b8a85dcfb7f17b43d0cf93d58ef40ae51f6aa37
kychilly <randomweebkeys@gmail.com>
2025-07-11 23:01:04 -0400

    emoji fix

--- src/main/java/com/Discord/DiscordBot/commands/RouletteCommand.java
+++ src/main/java/com/Discord/DiscordBot/commands/RouletteCommand.java
@@ -70,8 +70,8 @@ public class RouletteCommand {
         EmbedBuilder embed = new EmbedBuilder()
                 .setTitle("Sigma Roulette")
                 .setDescription(event.getUser().getAsMention() + ", you've loaded the toilet.\n\n**Rules:**\n" +
-                        "- If you get a **Chicken Jockey** <:chickenjockey:1389470392128639007> when you flush, you're safe!\n" +
-                        "- If you get a **Skibidi Toilet** <:SKIBIDITOILET:1389472754490671145> you lose.\n" +
+                        "- If you get a **Chicken Jockey** <:chickenjockey:1391619109308862625> when you flush, you're safe!\n" +
+                        "- If you get a **Skibidi Toilet** <:SKIBIDITOILET:1391619088970944522> you lose.\n" +
                         "- You can choose to spin or flush the toilet.\n" +
                         "Good luck!")
                 .setColor(Color.ORANGE);
@@ -110,7 +110,7 @@ public class RouletteCommand {
                 spinChamber(userId, user);//also prints the chamber to me
 
                 EmbedBuilder embed = new EmbedBuilder()
-                        .setTitle("<:chickenjockey:1389470392128639007> Sigma Roulette <:SKIBIDITOILET:1389472754490671145> - Toilet Spun")
+                        .setTitle("<:chickenjockey:1391619109308862625> Sigma Roulette <:SKIBIDITOILET:1391619088970944522> - Toilet Spun")
                         .setDescription(user.getAsMention() + " spun the toilet. Good luck!")
                         .addField("Times survived", String.valueOf(currentSurvive), false)
                         .setColor(Color.CYAN);
@@ -141,7 +141,7 @@ public class RouletteCommand {
                     surviveCount.remove(userId);
 
                     EmbedBuilder embed = new EmbedBuilder()
-                            .setTitle("<:SKIBIDITOILET:1389472754490671145> SKIBIDI SIGMA TOILET! You lost!")
+                            .setTitle("<:SKIBIDITOILET:1391619088970944522> SKIBIDI SIGMA TOILET! You lost!")
                             .setDescription(user.getAsMention() + " flushed the toilet and got flushed down! Game over.")
                             .addField("Total rounds survived", String.valueOf(currentSurvive), false)
                             .setColor(Color.RED);
@@ -156,7 +156,7 @@ public class RouletteCommand {
                     rotateChamber(userId);
 
                     EmbedBuilder embed = new EmbedBuilder()
-                            .setTitle("<:chickenjockey:1389470392128639007> CHICKEN JOCKEY!!! You're safe!")
+                            .setTitle("<:chickenjockey:1391619109308862625> CHICKEN JOCKEY!!! You're safe!")
                             .setDescription(user.getAsMention() + " flushed the toilet and and out came a chicken jockey <:chickenjockey:1389470392128639007>. Safe!")
                             .addField("Times survived", String.valueOf(currentSurvive), false)
                             .setColor(Color.GREEN);
